15e9c684d0f54b7ad296bbaf16c591910e52ac07
dongfu92 <718342645@qq.com>
2016-06-21 12:18:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dongfu/as_hellogit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dongfu.as_hellogit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
